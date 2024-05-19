8362c5386b33f82055626a8c456426e50a6d91de
Karol Stasiak <karsta26@gmail.com>
2024-05-19 15:23:20 +0200

    Added checkConfiguration functionality in BunRunConfiguration

--- src/main/kotlin/com/karsta26/bun/run/BunRunConfiguration.kt
+++ src/main/kotlin/com/karsta26/bun/run/BunRunConfiguration.kt
@@ -2,13 +2,12 @@ package com.karsta26.bun.run
 
 import com.intellij.execution.CommonProgramRunConfigurationParameters
 import com.intellij.execution.Executor
-import com.intellij.execution.configurations.ConfigurationFactory
-import com.intellij.execution.configurations.LocatableConfigurationBase
-import com.intellij.execution.configurations.RunConfiguration
-import com.intellij.execution.configurations.RunProfileState
+import com.intellij.execution.configurations.*
 import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.openapi.options.SettingsEditor
 import com.intellij.openapi.project.Project
+import java.io.File
+import java.nio.file.Path
 
 class BunRunConfiguration(project: Project, factory: ConfigurationFactory) :
     LocatableConfigurationBase<BunRunConfigurationOptions>(project, factory), CommonProgramRunConfigurationParameters {
@@ -45,4 +44,37 @@ class BunRunConfiguration(project: Project, factory: ConfigurationFactory) :
     override fun getWorkingDirectory() = options.workingDirectory
     override fun isPassParentEnvs() = options.isPassParentEnvs
     override fun getEnvs() = options.envs
+
+    override fun checkConfiguration() {
+        with(options) {
+            if (mySingleFileMode) {
+                if (workingDirectory == null || workingDirectory!!.isBlank()) {
+                    throw RuntimeConfigurationError("Please specify working directory")
+                }
+                if (!File(workingDirectory!!).isDirectory) {
+                    throw RuntimeConfigurationError("Please specify working directory correctly")
+                }
+                if (myJSFile == null || myJSFile!!.isBlank()) {
+                    throw RuntimeConfigurationError("Please specify JS/TS file")
+                }
+                if (Path.of(myJSFile!!).isAbsolute) {
+                    if (!Path.of(myJSFile!!).toFile().isFile) {
+                        throw RuntimeConfigurationError("Please specify JS/TS file correctly")
+                    }
+                } else {
+                    if (!Path.of(myWorkingDirectory!!, myJSFile).toFile().isFile) {
+                        throw RuntimeConfigurationError("JS/TS file not found in working directory")
+                    }
+                }
+            } else {
+                if (myPackageJsonPath == null || myPackageJsonPath!!.isBlank()) {
+                    throw RuntimeConfigurationError("Please specify package.json")
+                }
+                val file = File(myPackageJsonPath!!)
+                if (!file.isAbsolute || !file.isFile) {
+                    throw RuntimeConfigurationError("Please specify package.json correctly")
+                }
+            }
+        }
+    }
 }
